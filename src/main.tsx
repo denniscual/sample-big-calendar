64e1d7d6cb4fc0c5a67086bd952ba5a531524c09
Dennis Cual <denniscual0716@gmail.com>
2023-09-13 23:20:17 +1200

    import react-big-calendar css on main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "@/app";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
