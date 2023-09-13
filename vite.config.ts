60555a78d59e2ac241651eb006fc76afb484a284
Dennis Cual <denniscual0716@gmail.com>
2023-09-13 18:58:48 +1200

    proxy request for timeapi

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
+  server: {
+    proxy: {
+      "/api/countries": {
+        target: "https://timeapi.io/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace("api/countries", ""),
+      },
+    },
+  },
 });
